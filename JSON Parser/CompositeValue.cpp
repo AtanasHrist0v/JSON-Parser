a6551fd413ffa99039f173747e7e57d17be1ebd5
Atanas Hristov <the_doktora@abv.bg>
2023-06-15 17:18:05 +0300

    Wrote print() and clone().

--- JSON Parser/CompositeValue.cpp
+++ JSON Parser/CompositeValue.cpp
@@ -1,17 +1,36 @@
 #include "CompositeValue.h"
 
-void CompositeValue::print(unsigned tabsCount) const {}
+void CompositeValue::print(unsigned tabsCount) const {
+	std::cout << this->openingBracket << std::endl;
 
-void CompositeValue::search(const char* key) const {}
+	size_t vectorSize = this->data.getSize();
+	for (size_t i = 0; i < vectorSize; i++) {
+		this->data[i]->print(tabsCount + 1);
+	}
 
-void CompositeValue::set(const char* path, const char* value) {}
+	std::cout << std::endl << this->closingBracket;
+}
 
-void CompositeValue::create(const char* path, const char* value) {}
+void CompositeValue::search(const char* key) const {
 
-void CompositeValue::deleteElement(const char* path) {}
+}
 
-void CompositeValue::move(const char* from, const char* to) {}
+void CompositeValue::set(const char* path, const char* value) {
+
+}
+
+void CompositeValue::create(const char* path, const char* value) {
+
+}
+
+void CompositeValue::deleteElement(const char* path) {
+
+}
+
+void CompositeValue::move(const char* from, const char* to) {
+
+}
 
 Value* CompositeValue::clone() {
-	return nullptr;
+	return new CompositeValue(*this);
 }
