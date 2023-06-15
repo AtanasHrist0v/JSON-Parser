ef17244705d747a3bb4ed89dcc0b61e0cdbeee91
Atanas Hristov <the_doktora@abv.bg>
2023-06-15 13:08:57 +0300

    Added SimpleValue class.

---
+++ JSON Parser/SimpleValue.cpp
@@ -0,0 +1,17 @@
+#include "SimpleValue.h"
+
+void SimpleValue::print(unsigned tabsCount) const {}
+
+void SimpleValue::search(const char* key) const {}
+
+void SimpleValue::set(const char* path, const char* value) {}
+
+void SimpleValue::create(const char* path, const char* value) {}
+
+void SimpleValue::deleteElement(const char* path) {}
+
+void SimpleValue::move(const char* from, const char* to) {}
+
+Value* SimpleValue::clone() {
+	return new SimpleValue(*this);
+}
