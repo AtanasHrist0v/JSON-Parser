bb37bd9849ca235336e307d066836e2415f16ea1
Atanas Hristov <the_doktora@abv.bg>
2023-06-18 21:00:42 +0300

    Rewrote executeCommand() and run() with new Command logic.

--- JSON Parser/JSONParser.cpp
+++ JSON Parser/JSONParser.cpp
@@ -1,5 +1,9 @@
 #include "JSONParser.h"
 #include "Constants.h"
+#include "OpenCommand.h"
+#include "CloseCommand.h"
+#include "ExitCommand.h"
+#include "PrintCommand.h"
 #include <sstream>
 
 //static void clearConsole() {
@@ -20,93 +24,60 @@ static void removeQuotes(char* string) {
 }
 
 void JSONParser::executeCommand(const MyString& command, const Vector<SharedPtr<MyString>>& commandArguments, bool& commandIsExit) {
-	if (command == OPEN_COMMAND) {
-		//open(commandArguments);
-	} else if (command == EXIT_COMMAND) {
-		exit();
-		commandIsExit = true;
-	} /*else {
-		std::cout << INVALID_INPUT << std::endl;
-		pauseConsole();
-	}*/
+	static const ExitCommand exitCommand;
+	if (command == exitCommand.getName()) {
+		try {
+			exitCommand.execute(commandArguments, this->root, this->filePath);
+			commandIsExit = true;
+		} catch (const std::exception& ex) {
+			std::cout << "Error! " << ex.what() << std::endl;
+		}
 
-	if (&(*(this->root)) != nullptr) {
-		if (command == CLOSE_COMMAND) {
-			close();
-		} else if (command == SAVE_COMMAND) {
-			save();
-		} else if (command == SAVE_AS_COMMAND) {
-			saveAs();
-		} else if (command == VALIDATE_COMMAND) {
-			validate();
-		} else if (command == PRINT_COMMAND) {
-			this->root->print();
-		} else if (command == SEARCH_COMMAND) {
-			//this->root->search(commandArguments);
-		} else if (command == SET_COMMAND) {
-			//std::stringstream ss(commandArguments.c_str());
-			//char path[INPUT_MAX_SIZE]{};
-			//char value[INPUT_MAX_SIZE]{};
+		return;
+	}
 
-			//ss.getline(path, INPUT_MAX_SIZE, ' ');
-			//ss.getline(value, INPUT_MAX_SIZE);
-			//removeQuotes(path);
-			//removeQuotes(value);
-
-			//StringView pathView(path);
-
-			//this->root->set(pathView, value);
-		} else if (command == CREATE_COMMAND) {
-
-		} else if (command == DELETE_COMMAND) {
-
-		} else if (command == MOVE_COMMAND) {
-
+	static const size_t allCommandsCount = this->allCommands.getSize();
+	for (size_t i = 0; i < allCommandsCount; i++) {
+		if (allCommands[i]->getName() == command) {
+			try {
+				allCommands[i]->execute(commandArguments, this->root, this->filePath);
+			} catch (const std::exception& ex) {
+				std::cout << "Error! " << ex.what() << std::endl;
+			}
 		}
 	}
-	
-	// TODO
-	//else {
-	//	std::cout << INVALID_INPUT << std::endl;
-	//	pauseConsole();
-	//}
 }
 
-void JSONParser::open(const MyString& filePath) {
-	this->filePath = filePath;
+JSONParser::JSONParser() {
+	this->allCommands.push_back(std::move(SharedPtr<Command>(new OpenCommand)));
+	this->allCommands.push_back(std::move(SharedPtr<Command>(new CloseCommand)));
+	this->allCommands.push_back(std::move(SharedPtr<Command>(new ExitCommand)));
+	this->allCommands.push_back(std::move(SharedPtr<Command>(new PrintCommand)));
 }
-
-void JSONParser::close() {}
-
-void JSONParser::save() {}
-
-void JSONParser::saveAs() {}
-
-void JSONParser::exit() {}
-
-void JSONParser::validate() const {
-	std::ifstream ifs(this->filePath.c_str(), std::ios::in);
-
-	ifs.close();
-}
-
 
 void JSONParser::run() {
 	char userInput[INPUT_MAX_SIZE]{};
 	bool commandIsExit = false;
 
 	std::stringstream ss;
-	char command[INPUT_MAX_SIZE]{};
-	char commandArguments[INPUT_MAX_SIZE]{};
+	char commandStr[INPUT_MAX_SIZE]{};
+	char commandArgumentsStr[INPUT_MAX_SIZE]{};
+
+	Vector<SharedPtr<MyString>> commandArguments;
 
 	while (!commandIsExit) {
 		std::cout << '>';
 		std::cin.getline(userInput, INPUT_MAX_SIZE);
 
 		ss.str(userInput);
-		ss.getline(command, INPUT_MAX_SIZE, ' ');
-		ss.getline(commandArguments, INPUT_MAX_SIZE);
+		ss.getline(commandStr, INPUT_MAX_SIZE, ' ');
+		commandArguments.clear();
+		while (!ss.eof()) {
+			ss.getline(commandArgumentsStr, INPUT_MAX_SIZE);
+			commandArguments.push_back(std::move(SharedPtr<MyString>(new MyString(commandArgumentsStr))));
+		}
+		ss.clear();
 
-		//executeCommand(command, commandArguments, commandIsExit);
+		executeCommand(commandStr, commandArguments, commandIsExit);
 	}
 }
