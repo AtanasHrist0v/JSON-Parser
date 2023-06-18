f2a29d0025fc51f40cd4fb1b2c05da0da9cffb3b
Atanas Hristov <the_doktora@abv.bg>
2023-06-18 20:16:05 +0300

    Rewrote valueFactory() function.

--- JSON Parser/ValueFactory.cpp
+++ JSON Parser/ValueFactory.cpp
@@ -4,19 +4,67 @@
 #include "KeyValuePair.h"
 #include "CompositeValue.h"
 
-SharedPtr<Value> valueFactory(std::istream& is) {
-	char firstChar = is.peek();
-	
-	switch (firstChar) {
-		case VALUE_CHAR:
-			break;
+static char getClosingBracket(char openingBracket) {
+	switch (openingBracket) {
 		case CompositeValue::ARRAY_OPENING_BRACKET:
-			break;
+			return CompositeValue::ARRAY_CLOSING_BRACKET;
 		case CompositeValue::OBJECT_OPENING_BRACKET:
-			break;
+			return CompositeValue::OBJECT_CLOSING_BRACKET;
 		default:
 			break;
 	}
 
-	return SharedPtr<Value>();
+	return openingBracket;
+}
+
+SharedPtr<Value> valueFactory(std::istream& is, bool valueIsArray) {
+	static bool charIsWhiteSpace = false;
+
+	SharedPtr<Value> value;
+	char ch;
+
+	is >> ch;
+	if (ch == ',' || ch == ':') {
+		is >> ch;
+	}
+
+	switch (ch) {
+		case VALUE_CHAR:
+		{
+			if (valueIsArray) {
+				static char valueBuff[INPUT_MAX_SIZE]{};
+
+				is.getline(valueBuff, INPUT_MAX_SIZE, VALUE_CHAR);
+				value = new SimpleValue(valueBuff);
+
+				break;
+			}
+
+			static char keyBuff[INPUT_MAX_SIZE]{};
+			is.getline(keyBuff, INPUT_MAX_SIZE, VALUE_CHAR);
+
+			value = new KeyValuePair(keyBuff, valueFactory(is));
+		}
+		break;
+		case CompositeValue::ARRAY_OPENING_BRACKET:
+		case CompositeValue::OBJECT_OPENING_BRACKET:
+		{
+			Vector<SharedPtr<Value>> data;
+			char openingBracket = ch;
+			char closingBracket = getClosingBracket(ch);
+
+			do {
+				data.push_back(std::move(valueFactory(is, ch == CompositeValue::ARRAY_OPENING_BRACKET)));
+				is >> ch;
+			} while (ch != closingBracket);
+
+			value = new CompositeValue(std::move(data), openingBracket, closingBracket);
+		}
+		break;
+		default:
+			throw std::logic_error("JSON file is not in the correct format!");
+			break;
+	}
+
+	return value;
 }
