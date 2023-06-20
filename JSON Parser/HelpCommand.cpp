dba676860bd626c80223b27e54c98c07dcb52a3b
Atanas Hristov <the_doktora@abv.bg>
2023-06-20 14:17:56 +0300

    Rewrote constructor and execute() function.

--- JSON Parser/HelpCommand.cpp
+++ JSON Parser/HelpCommand.cpp
@@ -1,20 +1,34 @@
 #include "HelpCommand.h"
+#include "CommandsManager.h"
 
-HelpCommand::HelpCommand() {
-	this->name = "help";
-	this->argumentsCount = 0;
-}
+HelpCommand::HelpCommand() : Command("help", 1) {}
 
 void HelpCommand::execute(const Vector<SharedPtr<MyString>>& arguments, SharedPtr<Value>& root, MyString& filePath) const {
-	std::cout << "open" << std::endl;
-	std::cout << "close" << std::endl;
-	std::cout << "print" << std::endl;
-	std::cout << "exit" << std::endl;
+	const Vector<SharedPtr<Command>>& allCommands = CommandsManager::getInstance().getCommands();
+	size_t allCommandsCount = allCommands.getSize();
+	size_t argumentsCount = arguments.getSize();
+
+	if (argumentsCount == 0) {
+		for (size_t i = 0; i < allCommandsCount; i++) {
+			allCommands[i]->printDescription(10);//TODO -> remove magic number
+			std::cout << std::endl;
+		}
+		return;
+	}
+
+	for (size_t i = 0; i < allCommandsCount; i++) {
+		if (allCommands[i]->getName() == *arguments[i]) {
+			allCommands[i]->printInfo();
+			return;
+		}
+	}
+
+	throw std::logic_error("This command is not supported by the help utility.");
 }
 
 void HelpCommand::printDescription(unsigned descriptionAlignment) const {
-	std::cout << this->name;
-	size_t padding = descriptionAlignment - this->name.length();
+	std::cout << getName();
+	size_t padding = descriptionAlignment - getName().length();
 	for (size_t i = 0; i < padding; i++) {
 		std::cout << ' ';
 	}
