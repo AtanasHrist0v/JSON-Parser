edd1fb437b8529221402291619cbc7bfcb6dc1f0
Atanas Hristov <the_doktora@abv.bg>
2023-06-24 00:19:44 +0300

    Added ValidateCommand to CommandsManager.

--- JSON Parser/CommandsManager.cpp
+++ JSON Parser/CommandsManager.cpp
@@ -4,6 +4,7 @@
 #include "SaveCommand.h"
 #include "HelpCommand.h"
 #include "ExitCommand.h"
+#include "ValidateCommand.h"
 #include "PrintCommand.h"
 #include "SearchCommand.h"
 
@@ -13,6 +14,7 @@ CommandsManager::CommandsManager() {
 	this->commands.push_back(std::move(SharedPtr<Command>(new SaveCommand)));
 	this->commands.push_back(std::move(SharedPtr<Command>(new HelpCommand)));
 	this->commands.push_back(std::move(SharedPtr<Command>(new ExitCommand)));
+	this->commands.push_back(std::move(SharedPtr<Command>(new ValidateCommand)));
 	this->commands.push_back(std::move(SharedPtr<Command>(new PrintCommand)));
 	this->commands.push_back(std::move(SharedPtr<Command>(new SearchCommand)));
 }
